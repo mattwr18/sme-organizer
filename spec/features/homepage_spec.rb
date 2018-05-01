93da7e9f2e30e3eff8204323ac42a30446a07a4e
Matt Rider <mattwr18@gmail.com>
2018-05-01 16:46:10 -0300

    Add bootstrap, test the display of sales, purchases, and profit on hompage, navigation to sales and purchases pages

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -9,8 +9,17 @@ describe 'navigation' do
     expect(page.status_code).to eq(200)
   end
 
-  it 'has a title of Homepage' do
-    expect(page).to have_content(/Homepage/)
+  it 'has a title' do
+    expect(page).to have_content(/MissMoo/)
+  end
+
+  it 'has a total of all the sales' do
+    sale1 = FactoryBot.create(:sale)
+    sale2 = FactoryBot.create(:second_sale)
+
+    visit root_path
+
+    expect(page).to have_content(/Total sales: 21/)
   end
 
   it 'has total of all the purchases' do
@@ -20,5 +29,27 @@ describe 'navigation' do
     visit root_path
 
     expect(page).to have_content(/Total purchases: 35/)
+  end
+
+  it 'has the total profit' do
+    sale = FactoryBot.create(:sale)
+    purchase = FactoryBot.create(:purchase)
+
+    visit root_path
+
+    expect(page).to have_content(/Profit: -5/)
+  end
+
+  it 'has a link to the sales page' do
+    click_on "Sales"
+
+    expect(current_path).to eq(sales_path)
+  end
+
+  it 'has a link to the purchases page' do
+    click_on "Purchases"
+
+    expect(current_path).to eq(purchases_path)
+
   end
 end
